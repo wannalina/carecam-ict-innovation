075b31f9eb38e7b607ed32dc0bccce128206e95b
wannalina <annalina_wheeler@hotmail.com>
2025-06-06 14:03:28 +0200

    manage brief connection with device

--- raspberrypi/bluetooth_module/bluetooth.py
+++ raspberrypi/bluetooth_module/bluetooth.py
@@ -1,12 +1,63 @@
-from bleak import BleakScanner 	# bleak used for BLE / ideal for modern phones
+# import libs
+from bleak import BleakScanner, BleakClient 	# bleak used for BLE / ideal for modern phones
+import asyncio
+import RPi.GPIO as GPIO
+import time
 
 # function to discover bluetooth devices
 async def discover_devices():
     try:
-        while True:
-            devices = await BleakScanner.discover()
-            for device in devices:
-                print(f"Device {device.name} found with address: {device.address}")
+        devices = await BleakScanner.discover()
+        for device in devices:
+            print(f"Device {device.name} found with address: {device.address}")
         return devices
     except Exception as e:
         print(f"Error in device discovery: {e}")
+        return []
+
+# function to select and connect device
+async def select_and_connect_device(devices, select_pin, confirm_pin):
+    scroll_index = 0
+    last_selected_state = GPIO.input(select_pin)
+    last_confirm_state = GPIO.input(confirm_pin)
+    try:
+        if not devices:
+            print("No devices to pair. Please check Bluetooth settings.")
+            return
+
+        print("Use the button to 'scroll' through the list of devices. Select using the original button.")
+
+        while True:
+            device = devices[scroll_index]  # selected device
+            print(f"Selected device: {device.name} with address {device.address}")
+
+            # wait for button press
+            while True: 
+                select_state = GPIO.input(select_pin)   # select button state
+                confirm_state = GPIO.input(confirm_pin) # confirm button state
+
+                # select button pressed
+                if select_state == GPIO.LOW and last_selected_state == GPIO.HIGH:
+                    scroll_index = (scroll_index + 1) % len(devices)    #
+                    break
+
+                # confirm pressed
+                if confirm_state == GPIO.LOW and last_confirm_state == GPIO.HIGH:
+                    print(f"Attempting connection to '{device.name}'...")
+                    try: 
+                        async with BleakClient(device.address) as client:
+                            print("Device connected via Bluetooth successfully!")
+                            await asyncio.sleep(2)  #TODO: retrieve app data here
+                        return
+                    except Exception as e:
+                        print(f"Connection failed: {e}")
+                        return
+
+                # reset i/o
+                last_selected_state = select_state
+                last_confirm_state = confirm_state
+                time.sleep(0.5)
+
+    except Exception as e:
+        print(f"Error selecting and connecting to device: {e}")
+        return None
--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -4,47 +4,53 @@ import time
 import asyncio
 
 # import functions from libs
-from bluetooth_module.bluetooth import discover_devices
+from bluetooth_module.bluetooth import discover_devices, select_and_connect_device
 
 # pin config
 BLUETOOTH_BUTTON_PIN = 17	# corresponds to physical pin 11
-BLUETOOTH_LED_PIN = 12		# correpsonds to phsyical pin 32
+BLUETOOTH_LED_PIN = 12		# corresponds to phsyical pin 32
+SELECT_BUTTON_PIN = 2       # corresponds to physical pin 3
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BLUETOOTH_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SELECT_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BLUETOOTH_LED_PIN, GPIO.OUT)
 
-# function to listen to button press; returns boolean
-def button_press_action(button_state, output_pin):
+# function to listen to button press; returns int
+def button_press_action(button_state, output_pin, button_index):
     try:
-        if button_state == GPIO.HIGH:
-            # if button pressed turn on LED and return True
-            print("Button pressed.")
+        if (button_state == GPIO.HIGH):
+            # button pressed "on" or "off"
             GPIO.output(output_pin, GPIO.HIGH)
-            return True
+            button_index += 1   # switch button status
+            return button_index
         else:
-            # if button released turn off LED and return False
-            print("Button released.")
+            # button not pressed
             GPIO.output(output_pin, GPIO.LOW)
-            return False
-        time.sleep(0.1)
+            return button_index
+        time.sleep(0.2)
     except Exception as e:
         print(f"Error reading button input: {e}")
 
 # function to control application and run main program loop
 def main():
+    devices = []
+    button_index = 0    # even numbers "on"; odd numbers "off"
     try:
         while True:
             # get state of bluetooth button
             bluetooth_button_state = GPIO.input(BLUETOOTH_BUTTON_PIN)
-            is_bluetooth_on = button_press_action(bluetooth_button_state, BLUETOOTH_LED_PIN)
+            is_bluetooth_on = button_press_action(bluetooth_button_state, BLUETOOTH_LED_PIN, button_index)
 
             # if bluetooth button pressed, begin device discovery
             if is_bluetooth_on:
-                asyncio.run(discover_devices())		# discover bluetooth devices
+                devices = asyncio.run(discover_devices())
+                asyncio.run(select_and_connect_device(devices, SELECT_BUTTON_PIN, BLUETOOTH_BUTTON_PIN))
             else:
                 continue
+            print(f"Discovered devices: {devices}")
+            time.sleep(0.5)
 
     except Exception as e:
         print(f"Error running main application loop: {e}")
